b63daf39c56a1c67fe96321a2b5b489fbdcbc16e
anhnnhe176642 <anhnnhe176642@fpt.edu.vn>
2025-06-10 13:59:43 +0700

    fix: update routing to use BrowserRouter and set base path in Vite config

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./conext/AuthProvider.jsx";
 import { ChatProvider } from "./conext/ChatProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <ChatProvider>
-      <HashRouter>
+      <BrowserRouter basename="/giupviecvat">
         <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
           <App />
         </GoogleOAuthProvider>
-      </HashRouter>
+      </BrowserRouter>
     </ChatProvider>
   </AuthProvider>
 );
